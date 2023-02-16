164d19cf7a23d0091761e268ae733f2c1fa0b5bb
martinistuk-sc <martin.istuk@smartcode.eu>
2023-02-16 08:55:43 +0100

    dlService init

--- src/app/services/download/download.service.ts
+++ src/app/services/download/download.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
 	providedIn: "root",
 })
 export class DownloadService {
-	constructor() {}
+	constructor(private http: HttpClient) {}
+
+	public download(id: string) {
+		const fullPath: string =
+			"https://api.pixsell.hr/products/" +
+			id +
+			"/download/full/link?kind=PRINT&has_author_signature=false&source=PIXSELL";
+
+		this.http.post(fullPath, );
+	}
 }
